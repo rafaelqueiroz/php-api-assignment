279842ae00cfde7e146cbc6806653a1acf7e622f
Rafael Queiroz <rafaelfqf@gmail.com>
2019-04-29 16:45:08 -0300

    Implements VehicleController::index() and tests

--- app/Http/Controllers/VehicleController.php
+++ app/Http/Controllers/VehicleController.php
@@ -1,21 +1,45 @@
 <?php
-
 namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Clients\NCAPHttpClient;
+
 
 class VehicleController extends Controller
 {
+
+    /** @var NCAPHttpClient  */
+    private $client;
+
     /**
      * Create a new controller instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(NCAPHttpClient $NCAPClient)
     {
-        //
+        $this->client = $NCAPClient;
     }
 
-    public function index()
+    /**
+     *
+     */
+    public function index(Request $request)
     {
-        
+        $variants = $this->client->getVariants($request->modelYear, $request->manufacturer, $request->model);       
+        foreach ($variants->Results as &$variant) {
+            $variant->Description = $variant->VehicleDescription;
+            unset ($variant->VehicleDescription);
+            if ($request->withRating === 'true') {
+                $rating = $this->client->getSafetyRatings($variant->VehicleId);
+                $variant->CrashRating = $rating->Results[0]->OverallRating;
+            }
+        }
+
+        return response()->json([
+            'Count' => $variants->Count,
+            'Results' => $variants->Results
+        ]);     
     }
+
 }
--- tests/Http/Controllers/VehicleControllerTest.php
+++ tests/Http/Controllers/VehicleControllerTest.php
@@ -2,4 +2,46 @@
 class VehicleControllerTest extends TestCase
 {
 	
+    public function testRequirement1()
+    {
+        $response = $this->get('/vehicles/2015/Audi/A3');
+        $response->seeJson(['Count' => 4, 'Results' => [["Description" =>"2015 Audi A3 4 DR AWD","VehicleId"=>9403],["Description"=>"2015 Audi A3 4 DR FWD","VehicleId"=>9408],["Description"=>"2015 Audi A3 C AWD","VehicleId"=>9405],["Description"=>"2015 Audi A3 C FWD","VehicleId"=>9406]]]);
+
+        $response = $this->get('/vehicles/2015/Toyota/Yaris');
+        $response->seeJson(["Count"=>2,"Results"=>[["Description"=>"2015 Toyota Yaris 3 HB FWD","VehicleId"=>9791],["Description"=>"2015 Toyota Yaris Liftback 5 HB FWD","VehicleId"=>9146]]]);
+
+        $response = $this->get('/vehicles/2015/Ford/Crown');
+        $response->seeJson(["Count"=>0,"Results"=>[]]);
+
+        $response = $this->get('/vehicles/undefined/Ford/Fusion');
+        $response->seeJson(["Count"=>0,"Results"=>[]]);
+    }
+
+    public function testRequirement2()
+    {
+        $response = $this->post('/vehicles', ['modelYear' => 2015, 'manufacturer' => 'Audi', 'model' => 'A3']);
+        $response->seeJson(['Count' => 4, 'Results' => [["Description" =>"2015 Audi A3 4 DR AWD","VehicleId"=>9403],["Description"=>"2015 Audi A3 4 DR FWD","VehicleId"=>9408],["Description"=>"2015 Audi A3 C AWD","VehicleId"=>9405],["Description"=>"2015 Audi A3 C FWD","VehicleId"=>9406]]]);
+
+        $response = $this->post('/vehicles', ['modelYear' => 2015, 'manufacturer' => 'Toyota', 'model' => 'Yaris']);
+        $response->seeJson(["Count"=>2,"Results"=>[["Description"=>"2015 Toyota Yaris 3 HB FWD","VehicleId"=>9791],["Description"=>"2015 Toyota Yaris Liftback 5 HB FWD","VehicleId"=>9146]]]);
+
+        $response = $this->post('/vehicles', ['manufacturer' => 'Honda', 'model' => 'Accord']);
+     	$response->seeJson(["Count"=>0,"Results"=>[]]);
+    }
+
+    public function testRequirement3()
+    {
+        $response = $this->get('/vehicles/2015/Audi/A3?withRating=true');
+        $response->seeJson(["Count"=>4,"Results"=>[["Description"=>"2015 Audi A3 4 DR AWD","VehicleId"=>9403,"CrashRating"=>"5"],["Description"=>"2015 Audi A3 4 DR FWD","VehicleId"=>9408,"CrashRating"=>"5"],["Description"=>"2015 Audi A3 C AWD","VehicleId"=>9405,"CrashRating"=>"Not Rated"],["Description"=>"2015 Audi A3 C FWD","VehicleId"=>9406,"CrashRating"=>"Not Rated"]]]);
+
+        $response = $this->get('/vehicles/2015/Audi/A3?withRating=false');
+        $response->seeJson(['Count' => 4, 'Results' => [["Description" =>"2015 Audi A3 4 DR AWD","VehicleId"=>9403],["Description"=>"2015 Audi A3 4 DR FWD","VehicleId"=>9408],["Description"=>"2015 Audi A3 C AWD","VehicleId"=>9405],["Description"=>"2015 Audi A3 C FWD","VehicleId"=>9406]]]);
+
+        $response = $this->get('/vehicles/2015/Audi/A3?withRating=bananas');
+        $response->seeJson(['Count' => 4, 'Results' => [["Description" =>"2015 Audi A3 4 DR AWD","VehicleId"=>9403],["Description"=>"2015 Audi A3 4 DR FWD","VehicleId"=>9408],["Description"=>"2015 Audi A3 C AWD","VehicleId"=>9405],["Description"=>"2015 Audi A3 C FWD","VehicleId"=>9406]]]);
+
+        $response = $this->get('/vehicles/2015/Audi/A3');
+        $response->seeJson(['Count' => 4, 'Results' => [["Description" =>"2015 Audi A3 4 DR AWD","VehicleId"=>9403],["Description"=>"2015 Audi A3 4 DR FWD","VehicleId"=>9408],["Description"=>"2015 Audi A3 C AWD","VehicleId"=>9405],["Description"=>"2015 Audi A3 C FWD","VehicleId"=>9406]]]);
+    }
+   
 }
